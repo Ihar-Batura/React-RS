210644250c7dbb1b4fb020e6c12530a1a2bd0b12
Ihar-Batura <A17331582@gmail.com>
2025-01-29 18:22:15 +0300

    feat: add component Footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,23 @@
+import { Component } from 'react';
+import type { ReactNode } from 'react';
+import styles from './index.module.css';
+
+export class Footer extends Component {
+  render(): ReactNode {
+    return (
+      <footer className={styles.footer}>
+        <div className={styles.wrapper}>
+          <p className={styles.year}>© 2025</p>
+          <a
+            className={styles.link}
+            href="https://github.com/Ihar-Batura"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Ihar Batura
+          </a>
+        </div>
+      </footer>
+    );
+  }
+}
