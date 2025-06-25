ac0ae79cf5f0bc350a3bee91f32fa384cc143661
Ihar-Batura <A17331582@gmail.com>
2025-06-25 14:13:43 +0300

    feat: add Spinner.tsx

---
+++ src/components/ui/spinner/Spinner.tsx
@@ -0,0 +1,13 @@
+import styles from './Spinner.module.scss';
+import { Component } from 'react';
+import type { ReactNode } from 'react';
+
+export class Spinner extends Component {
+  render(): ReactNode {
+    return (
+      <div className={styles.container}>
+        <div className={styles.spinner}></div>
+      </div>
+    );
+  }
+}
