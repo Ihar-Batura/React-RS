b178552b3a01700afbe72afb3e5452c92511e8f6
Ihar-Batura <A17331582@gmail.com>
2025-07-04 14:48:49 +0300

    refactor: migrate Footer to functional component

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,23 +1,19 @@
-import { Component } from 'react';
-import type { ReactNode } from 'react';
 import styles from './Footer.module.scss';
 
-export class Footer extends Component {
-  render(): ReactNode {
-    return (
-      <footer className={styles.footer}>
-        <div className={styles.wrapper}>
-          <p className={styles.year}>{`© ${new Date().getFullYear()}`}</p>
-          <a
-            className={styles.link}
-            href="https://github.com/Ihar-Batura"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            Ihar Batura
-          </a>
-        </div>
-      </footer>
-    );
-  }
-}
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.wrapper}>
+        <p className={styles.year}>{`© ${new Date().getFullYear()}`}</p>
+        <a
+          className={styles.link}
+          href="https://github.com/Ihar-Batura"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Ihar Batura
+        </a>
+      </div>
+    </footer>
+  );
+};
