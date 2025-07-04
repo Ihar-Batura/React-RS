8868720016e51748d4ca8e581ad39f60efdbe0b8
Ihar-Batura <A17331582@gmail.com>
2025-07-04 15:20:49 +0300

    refactor: migrate SearchPage to functional component

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,76 +1,62 @@
-import { Component } from 'react';
-import type { ReactNode } from 'react';
+import { useState, useEffect } from 'react';
 import styles from './SearchPage.module.scss';
 import { Search } from '../../components/common/search/Search';
 import { Results } from '../../components/common/results/Results';
-import { apiSearchCharacters } from '../../shared/api/apiSearchCharacters';
+import apiSearchCharacters from '../../shared/api/apiSearchCharacters';
 import { CharactersResponse } from '../../shared/types/types';
 import getLastSearchTermFromLS from '../../shared/ls/getLastSearchTermFormLS';
 
-interface SearchPageState {
-  searchTerm: string;
-  searchResults: CharactersResponse | null;
-  searchError: boolean;
-  isLoading: boolean;
-  hasTestError: boolean;
-}
+export const SearchPage = () => {
+  const [searchTerm, setSearchTerm] = useState<string>(
+    getLastSearchTermFromLS() || ''
+  );
+  const [searchResults, setSearchResults] = useState<CharactersResponse | null>(
+    null
+  );
+  const [searchError, setSearchError] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [hasTestError, setHasTestError] = useState<boolean>(false);
 
-export class SearchPage extends Component {
-  state: SearchPageState = {
-    searchTerm: '',
-    searchResults: null,
-    searchError: false,
-    isLoading: false,
-    hasTestError: false,
-  };
+  useEffect(() => {
+    handleSearch(searchTerm);
+  }, []);
 
-  componentDidMount(): void {
-    const lastTerm = getLastSearchTermFromLS();
-    const term = lastTerm ?? '';
-    this.handleSearch(term);
-  }
-
-  handleSearch = async (term: string): Promise<void> => {
-    this.setState({ isLoading: true });
+  const handleSearch = async (term: string): Promise<void> => {
+    setIsLoading(true);
+    setSearchError(false);
     try {
       const results = await apiSearchCharacters(term.trim());
-
-      this.setState({
-        searchResults: results,
-        searchTerm: term,
-      });
+      setSearchTerm(term);
+      setSearchResults(results);
+      setSearchError(false);
     } catch (error) {
       console.error('Failed to fetch characters:', error);
-      this.setState({ searchError: true });
+      setSearchError(true);
     } finally {
-      this.setState({ isLoading: false });
+      setIsLoading(false);
     }
   };
 
-  throwTestError = (): void => {
-    this.setState({
-      hasTestError: true,
-    });
+  const throwTestError = (): void => {
+    setHasTestError(true);
   };
 
-  render(): ReactNode {
-    if (this.state.hasTestError) {
-      throw new Error("Don't worry this a test error");
-    }
-    return (
-      <main className={styles.container}>
-        <div className={styles.wrapper}>
-          <Search onSearch={this.handleSearch} />
-          <Results
-            data={this.state.searchResults}
-            isError={this.state.searchError}
-            isLoading={this.state.isLoading}
-          />
-          <button className={styles.button} onClick={this.throwTestError}>
-            Error Button
-          </button>
-        </div>
-      </main>
-    );
+  if (hasTestError) {
+    throw new Error("Don't worry this a test error");
   }
-}
+  return (
+    <main className={styles.container}>
+      <div className={styles.wrapper}>
+        <Search onSearch={handleSearch} />
+        <Results
+          data={searchResults}
+          isError={searchError}
+          isLoading={isLoading}
+        />
+        <button className={styles.button} onClick={throwTestError}>
+          Error Button
+        </button>
+      </div>
+    </main>
+  );
+};
