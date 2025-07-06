9d048368370eab7097e285905043a00c88ebc257
Ihar-Batura <A17331582@gmail.com>
2025-07-06 12:01:33 +0300

    feat: add navigation to header

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,10 +1,22 @@
 import styles from './Header.module.scss';
+import { useNavigate } from 'react-router';
+
+import { Navigate } from '../../ui/navigate/Navigate';
 
 export const Header = () => {
+  const navigate = useNavigate();
+
+  const handleGoHome = () => {
+    navigate('/');
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.wrapper}>
-        <h1 className={styles.logo}>Star Trek</h1>
+        <h1 className={styles.logo} onClick={handleGoHome}>
+          Star Trek
+        </h1>
+        <Navigate />
       </div>
     </header>
   );
