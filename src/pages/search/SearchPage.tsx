3afb0833c45e982e235dcafe56b6804e1a232d38
Ihar-Batura <A17331582@gmail.com>
2025-07-06 11:59:59 +0300

    feat: add pagination to SearchPage

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -5,6 +5,8 @@ import { Results } from '../../components/common/results/Results';
 import apiSearchCharacters from '../../shared/api/apiSearchCharacters';
 import { CharactersResponse } from '../../shared/types/types';
 import { useLocalStorageSearchTerm } from '../../shared/hooks/useLocalStorageSearchTerm';
+import { Pagination } from '../../components/common/pagination/Pagination';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 export const SearchPage = () => {
   const [searchTerm, setSearchTerm] = useLocalStorageSearchTerm();
@@ -13,20 +15,44 @@ export const SearchPage = () => {
   );
   const [searchError, setSearchError] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [hasTestError, setHasTestError] = useState<boolean>(false);
+
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const currentPageFromUrl = Number(searchParams.get('page')) || 1;
+  const [currentPage, setCurrentPage] = useState(currentPageFromUrl);
 
   useEffect(() => {
-    handleSearch(searchTerm);
-  }, []);
+    const newPage = Number(searchParams.get('page')) || 1;
+    setCurrentPage(newPage);
+  }, [searchParams]);
 
-  const handleSearch = async (term: string): Promise<void> => {
+  useEffect(() => {
+    if (currentPage !== currentPageFromUrl) {
+      searchParams.set('page', currentPage.toString());
+      navigate(`?${searchParams.toString()}`, { replace: true });
+    }
+  }, [currentPage, currentPageFromUrl, navigate, searchParams]);
+
+  useEffect(() => {
+    handleSearch(searchTerm, currentPage);
+  }, [currentPage]);
+
+  const handleSearch = async (term: string, page: number): Promise<void> => {
     setIsLoading(true);
     setSearchError(false);
     try {
-      const results = await apiSearchCharacters(term.trim());
+      const actualPage = searchTerm !== term ? 1 : page;
+
+      const results = await apiSearchCharacters(term.trim(), actualPage - 1);
+
       setSearchTerm(term);
       setSearchResults(results);
       setSearchError(false);
+
+      const newSearchParams = new URLSearchParams(searchParams);
+      newSearchParams.set('page', actualPage.toString());
+      navigate(`?${newSearchParams.toString()}`, { replace: true });
     } catch (error) {
       console.error('Failed to fetch characters:', error);
       setSearchError(true);
@@ -35,25 +61,23 @@ export const SearchPage = () => {
     }
   };
 
-  const throwTestError = (): void => {
-    setHasTestError(true);
-  };
-
-  if (hasTestError) {
-    throw new Error("Don't worry this a test error");
-  }
   return (
     <main className={styles.container}>
       <div className={styles.wrapper}>
-        <Search onSearch={handleSearch} />
+        <Search onSearch={(term) => handleSearch(term, 1)} />
         <Results
           data={searchResults}
           isError={searchError}
           isLoading={isLoading}
         />
-        <button className={styles.button} onClick={throwTestError}>
-          Error Button
-        </button>
+        {searchResults?.page.totalPages &&
+          searchResults.page.totalPages > 1 && (
+            <Pagination
+              currentPage={currentPage}
+              totalPages={searchResults.page.totalPages}
+              onPageChange={setCurrentPage}
+            />
+          )}
       </div>
     </main>
   );
