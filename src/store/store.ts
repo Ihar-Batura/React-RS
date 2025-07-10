a59f92058a3e3020764591628039fceb0f461a7c
Ihar-Batura <A17331582@gmail.com>
2025-07-10 11:28:30 +0300

    feat: add reducer selectedItems

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import selectedItemsSlice from './selectedItemsSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    selectedItems: selectedItemsSlice,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
