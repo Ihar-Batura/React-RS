164aefe54bb2fa1f3dd9a9f3aa7cbeb846d723b7
Ihar-Batura <A17331582@gmail.com>
2025-07-10 16:08:51 +0300

    feat: add button change theme to navigate

--- src/components/ui/navigate/Navigate.tsx
+++ src/components/ui/navigate/Navigate.tsx
@@ -1,8 +1,14 @@
 import styles from './Navigate.module.scss';
 import { useNavigate } from 'react-router';
+import { useTheme } from '../../../shared/hooks/useTheme';
 
 export const Navigate = () => {
   const navigate = useNavigate();
+  const { theme, toggleTheme } = useTheme();
+
+  const changeTheme = () => {
+    toggleTheme();
+  };
 
   const handleGoAbout = () => {
     navigate('/about');
@@ -13,6 +19,9 @@ export const Navigate = () => {
       <ul className={styles.itemList}>
         <li className={styles.item} onClick={handleGoAbout}>
           About
+        </li>
+        <li className={styles.item} onClick={changeTheme}>
+          {theme === 'light' ? 'Dark' : 'Light'}
         </li>
       </ul>
     </nav>
