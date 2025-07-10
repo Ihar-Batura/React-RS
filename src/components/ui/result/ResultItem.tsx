d2b8ff6ae320d2ebafdbb58ef9ef1df87187e667
Ihar-Batura <A17331582@gmail.com>
2025-07-10 11:30:14 +0300

    feat: add checkbox to ResultItem

--- src/components/ui/result/ResultItem.tsx
+++ src/components/ui/result/ResultItem.tsx
@@ -1,9 +1,18 @@
 import styles from './ResultItem.module.scss';
 import { Character } from '../../../shared/types/types';
+import { useSelector, useDispatch } from 'react-redux';
+import { addItem, removeItem } from '../../../store/selectedItemsSlice';
+import { RootState } from '../../../store/store';
 
 interface ResultItemProps {
   itemData: Character;
   onSelect: () => void;
+}
+
+interface SelectedItem {
+  id: string;
+  name: string;
+  description: string;
 }
 
 export const ResultItem = ({ itemData, onSelect }: ResultItemProps) => {
@@ -18,10 +27,42 @@ export const ResultItem = ({ itemData, onSelect }: ResultItemProps) => {
     ''
   );
 
+  const dispatch = useDispatch();
+
+  const selectedItems = useSelector(
+    (state: RootState) => state.selectedItems.selectedItems
+  );
+  const isSelected = selectedItems.some(
+    (item: SelectedItem) => item.id === itemData.uid
+  );
+
+  const handleCheckboxChange = () => {
+    if (isSelected) {
+      dispatch(removeItem({ id: itemData.uid }));
+    } else {
+      dispatch(
+        addItem({
+          id: itemData.uid,
+          name: itemData.name,
+          description: descriptions,
+        })
+      );
+    }
+  };
+
   return (
     <li className={styles.container} onClick={onSelect}>
       <p className={styles.name}>{itemData.name}</p>
       <p className={styles.description}>{descriptions}</p>
+      <input
+        className={styles.flag}
+        type="checkbox"
+        checked={isSelected}
+        onChange={handleCheckboxChange}
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      />
     </li>
   );
 };
