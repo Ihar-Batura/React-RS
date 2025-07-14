4ecf4b7fd51148fd58e3763f583326ec87f48efa
Ihar-Batura <A17331582@gmail.com>
2025-07-14 11:30:18 +0300

    feat: add rtk-hooks and useGetCharacterByUidQuery and useSearchCharactersQuery

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,80 +1,44 @@
-import { useState, useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router';
 import styles from './SearchPage.module.scss';
 import { Search } from '../../components/common/search/Search';
 import { Results } from '../../components/common/results/Results';
-import apiSearchCharacters from '../../shared/api/apiSearchCharacters';
-import { CharactersResponse, Character } from '../../shared/types/types';
 import { useLocalStorageSearchTerm } from '../../shared/hooks/useLocalStorageSearchTerm';
 import { Pagination } from '../../components/common/pagination/Pagination';
-import apiGetCharacterData from '../../shared/api/apiGetCharacterData';
 import { Spinner } from '../../components/ui/spinner/Spinner';
 import { CharacterDetails } from '../../components/ui/character/CharacterDetails';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
 import { PopupChoseElements } from '../../components/ui/popup/PopupChoseElements';
 import { useTheme } from '../../shared/hooks/useTheme';
+import { useGetCharacterByUidQuery } from '../../store/apiSlice';
+import { useSearchCharactersQuery } from '../../store/apiSlice';
 
 export const SearchPage = () => {
   const { theme } = useTheme();
 
   const [searchTerm, setSearchTerm] = useLocalStorageSearchTerm();
-  const [searchResults, setSearchResults] = useState<CharactersResponse | null>(
-    null
-  );
-  const [searchError, setSearchError] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const [detailsData, setDetailsData] = useState<Character | null>(null);
-  const [detailsLoading, setDetailsLoading] = useState(false);
-
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
 
   const currentPage = Number(searchParams.get('page')) || 1;
   const detailsUidFromUrl = searchParams.get('details');
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      try {
-        const results = await apiSearchCharacters(
-          searchTerm.trim(),
-          currentPage - 1
-        );
-        setSearchResults(results);
-      } catch (error) {
-        console.error('Failed to fetch characters:', error);
-        setSearchError(true);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+  const {
+    data: searchResults,
+    isLoading: searchLoading,
+    isError: searchError,
+  } = useSearchCharactersQuery({
+    term: searchTerm.trim(),
+    page: currentPage - 1,
+  });
 
-    fetchData();
-  }, [searchTerm, currentPage]);
-
-  useEffect(() => {
-    if (!detailsUidFromUrl) {
-      setDetailsData(null);
-      return;
-    }
-
-    const fetchDetails = async () => {
-      setDetailsLoading(true);
-
-      try {
-        const data = await apiGetCharacterData(detailsUidFromUrl);
-        setDetailsData(data);
-      } catch (error) {
-        console.error('Failed to load character details', error);
-      } finally {
-        setDetailsLoading(false);
-      }
-    };
-
-    fetchDetails();
-  }, [detailsUidFromUrl]);
+  const {
+    data: detailsData,
+    isLoading: detailsLoading,
+    isError: detailsError,
+  } = useGetCharacterByUidQuery(detailsUidFromUrl || '', {
+    skip: !detailsUidFromUrl,
+  });
 
   const handleSelectCharacter = (uid: string) => {
     const newSearchParams = new URLSearchParams(searchParams);
@@ -86,7 +50,6 @@ export const SearchPage = () => {
     const newSearchParams = new URLSearchParams(searchParams);
     newSearchParams.delete('details');
     navigate(`?${newSearchParams.toString()}`);
-    setDetailsData(null);
   };
 
   const handleSearch = (term: string) => {
@@ -114,9 +77,9 @@ export const SearchPage = () => {
         <div className={styles.searchSection}>
           <Search onSearch={handleSearch} />
           <Results
-            data={searchResults}
+            data={searchResults ?? null}
             isError={searchError}
-            isLoading={isLoading}
+            isLoading={searchLoading}
             onSelectCharacter={handleSelectCharacter}
           />
           {searchResults?.page?.totalPages &&
@@ -136,8 +99,10 @@ export const SearchPage = () => {
             </button>
             {detailsLoading ? (
               <Spinner />
+            ) : detailsError ? (
+              <div>Error loading character details</div>
             ) : (
-              <CharacterDetails character={detailsData} />
+              <CharacterDetails character={detailsData ?? null} />
             )}
           </div>
         )}
