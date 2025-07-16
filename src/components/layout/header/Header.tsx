984335d0a4058a8c6f8629640b654dcb569dcd93
Ihar-Batura <A17331582@gmail.com>
2025-07-16 16:49:03 +0300

    feat: add Link to header

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,21 +1,20 @@
 import styles from './Header.module.scss';
-import { useNavigate } from 'react-router';
+
 import { useTheme } from '../../../shared/hooks/useTheme';
 import { Navigate } from '../../ui/navigate/Navigate';
+import Link from 'next/link';
 
 export const Header = () => {
   const { theme } = useTheme();
-  const navigate = useNavigate();
-
-  const handleGoHome = () => {
-    navigate('/');
-  };
 
   return (
     <header className={`${styles.header} ${styles[theme]}`}>
       <div className={styles.wrapper}>
-        <h1 className={styles.logo} onClick={handleGoHome}>
-          Star Trek
+        <h1>
+          <Link className={styles.logo} href="/">
+            {' '}
+            Star Trek
+          </Link>
         </h1>
         <Navigate />
       </div>
