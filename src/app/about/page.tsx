315cd67e06c5a9663af715bce7ae9fd9d91b6246
Ihar-Batura <A17331582@gmail.com>
2025-07-17 08:37:06 +0300

    feat: add page About

---
+++ src/app/about/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { Header } from '../../components/layout/header/Header';
+import { About } from '../../pages/about/About';
+import { Footer } from '../../components/layout/footer/Footer';
+
+export default function AboutPage() {
+  return (
+    <>
+      <Header />
+      <About />
+      <Footer />
+    </>
+  );
+}
