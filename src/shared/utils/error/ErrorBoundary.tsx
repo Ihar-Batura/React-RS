58900d30054b4bbfd969b32e2464a99b1884ad8b
Ihar-Batura <A17331582@gmail.com>
2025-07-22 17:23:06 +0300

    feat: try to add useTranslations for error boundary

--- src/shared/utils/error/ErrorBoundary.tsx
+++ src/shared/utils/error/ErrorBoundary.tsx
@@ -3,16 +3,20 @@
 import React from 'react';
 import styles from './ErrorBoundary.module.scss';
 import { ThemeContext } from '../../../context/themeContext';
+import { useTranslations } from 'next-intl';
 
 interface ErrorBoundaryProps {
   children: React.ReactNode;
+  errorTitle?: string;
+  errorDescription?: string;
+  buttonText?: string;
 }
 
 interface ErrorBoundaryState {
   hasError: boolean;
 }
 
-export class ErrorBoundary extends React.Component<
+export class ErrorBoundaryClass extends React.Component<
   ErrorBoundaryProps,
   ErrorBoundaryState
 > {
@@ -32,7 +36,13 @@ export class ErrorBoundary extends React.Component<
     console.error('Error caught by Error Boundary:', error, errorInfo);
   }
 
+  handleReset = () => {
+    this.setState({ hasError: false });
+    window.location.href = '/';
+  };
+
   render() {
+    const { errorTitle, errorDescription, buttonText } = this.props;
     const context = this.context;
 
     if (!context) {
@@ -45,15 +55,40 @@ export class ErrorBoundary extends React.Component<
       this.props.children
     ) : (
       <div className={`${styles.container} ${styles[theme]}`}>
-        <h2 className={styles.title}>Something went wrong...</h2>
-        <p className={styles.description}>Don&apos;t worry!</p>
-        <button
-          className={styles.button}
-          onClick={() => this.setState({ hasError: false })}
-        >
-          Try again
+        <h2 className={styles.title}>{errorTitle}</h2>
+        <p className={styles.description}>{errorDescription}</p>
+        <button className={styles.button} onClick={this.handleReset}>
+          {buttonText}
         </button>
       </div>
     );
   }
 }
+
+const ErrorBoundary: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  let errorTitle = 'Something went wrong...';
+  let errorDescription = 'Don&apos;t worry!';
+  let buttonText = 'Try again';
+  const t = useTranslations('ErrorBoundary');
+
+  try {
+    errorTitle = t('title');
+    errorDescription = t('description');
+    buttonText = t('buttonText');
+  } catch (e) {
+    console.warn('Failed to load translations for ErrorBoundary', e);
+  }
+  return (
+    <ErrorBoundaryClass
+      errorTitle={errorTitle}
+      errorDescription={errorDescription}
+      buttonText={buttonText}
+    >
+      {children}
+    </ErrorBoundaryClass>
+  );
+};
+
+export default ErrorBoundary;
