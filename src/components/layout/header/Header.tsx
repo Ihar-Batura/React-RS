bb7617012de0d269b13f5dc7318ac100faf63bdd
Ihar-Batura <A17331582@gmail.com>
2025-07-22 11:00:17 +0300

    feat: add useTranslations to header

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -3,16 +3,18 @@ import styles from './Header.module.scss';
 import { useTheme } from '../../../shared/hooks/useTheme';
 import { Navigate } from '../../ui/navigate/Navigate';
 import Link from 'next/link';
+import { useTranslations } from 'next-intl';
 
 export const Header = () => {
   const { theme } = useTheme();
+  const t = useTranslations('Header');
 
   return (
     <header className={`${styles.header} ${styles[theme]}`}>
       <div className={styles.wrapper}>
         <h1>
           <Link className={styles.logo} href="/">
-            Star Trek
+            {t('logo')}
           </Link>
         </h1>
         <Navigate />
