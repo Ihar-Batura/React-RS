b450cb15c5eec86e5c71a5b76631e6cc5ad376e8
Ihar-Batura <A17331582@gmail.com>
2025-07-22 12:09:39 +0300

    feat: add button change language to Navigate

--- src/components/ui/navigate/Navigate.tsx
+++ src/components/ui/navigate/Navigate.tsx
@@ -3,12 +3,26 @@
 import styles from './Navigate.module.scss';
 import { useTheme } from '../../../shared/hooks/useTheme';
 import Link from 'next/link';
+import { useTranslations, useLocale } from 'next-intl';
+import { usePathname, useRouter } from '../../../i18n/navigation';
 
 export const Navigate = () => {
   const { theme, toggleTheme } = useTheme();
+  const t = useTranslations('Navigate');
+  const currentLocale = useLocale();
+
+  const pathname = usePathname();
+  const router = useRouter();
 
   const changeTheme = () => {
     toggleTheme();
+  };
+
+  const changeLanguage = () => {
+    router.replace(
+      { pathname },
+      { locale: currentLocale === 'en' ? 'ru' : 'en' }
+    );
   };
 
   return (
@@ -16,11 +30,14 @@ export const Navigate = () => {
       <ul className={styles.itemList}>
         <li className={styles.item}>
           <Link className={styles.link} href="/about">
-            About
+            {t('btnAbout')}
           </Link>
         </li>
         <li className={styles.link} onClick={changeTheme}>
-          {theme === 'light' ? 'Dark' : 'Light'}
+          {theme === 'light' ? t('btnThemeLight') : t('btnThemeDark')}
+        </li>
+        <li className={styles.link} onClick={changeLanguage}>
+          {t('btnLang')}
         </li>
       </ul>
     </nav>
