c80d519eae0916b0d345a846e7f6f6cb9247bbea
Ihar-Batura <A17331582@gmail.com>
2025-07-24 11:07:22 +0300

    feat: add ClientProviders

---
+++ src/app/ClientProviders.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { ThemeProvider } from '../context/ThemeProvider';
+import { Provider } from 'react-redux';
+import { store } from '../store/store';
+import { Header } from '../components/layout/header/Header';
+import { Footer } from '../components/layout/footer/Footer';
+
+export default function ClientProviders({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <Provider store={store}>
+      <ThemeProvider>
+        <Header />
+        {children}
+        <Footer />
+      </ThemeProvider>
+    </Provider>
+  );
+}
