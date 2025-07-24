c252678f144fc646aecfae9886bd2b9e23550c43
Ihar-Batura <A17331582@gmail.com>
2025-07-24 11:07:04 +0300

    fix: change layout.tsx in locale and add ClientProviders

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,8 +1,8 @@
 import { NextIntlClientProvider, hasLocale } from 'next-intl';
 import { notFound } from 'next/navigation';
 import { routing } from '../../i18n/routing';
-import ErrorBoundary from '../../shared/utils/error/ErrorBoundary';
 import { Messages } from '../../shared/types/types';
+import ClientProviders from '../ClientProviders';
 
 export default async function LocaleLayout({
   children,
@@ -20,18 +20,8 @@ export default async function LocaleLayout({
     .default;
 
   return (
-    <html lang={locale}>
-      <head>
-        <title>{locale === 'en' ? 'Star Trek' : 'Звездный путь'}</title>
-        <meta name="description" content="Star Trek application" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/star-trek-logo.svg" />
-      </head>
-      <body>
-        <NextIntlClientProvider messages={messages}>
-          <ErrorBoundary>{children}</ErrorBoundary>
-        </NextIntlClientProvider>
-      </body>
-    </html>
+    <NextIntlClientProvider messages={messages}>
+      <ClientProviders>{children}</ClientProviders>
+    </NextIntlClientProvider>
   );
 }
