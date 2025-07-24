907dd6826ad06080a7230e8fd6655d4e3c7f8737
Ihar-Batura <A17331582@gmail.com>
2025-07-24 14:53:10 +0300

    refactor: change ButtonGoBack

--- src/components/ui/buttons/go-back.tsx
+++ src/components/ui/buttons/go-back.tsx
@@ -2,8 +2,13 @@
 
 import styles from './buttons.module.scss';
 import { useRouter } from 'next/navigation';
+import { useTheme } from '../../../shared/hooks/useTheme';
+import { useTranslations } from 'next-intl';
 
 export default function ButtonGoBack() {
+  const { theme } = useTheme();
+  const t = useTranslations('About');
+
   const router = useRouter();
 
   const handleGoBack = () => {
@@ -11,8 +16,12 @@ export default function ButtonGoBack() {
   };
 
   return (
-    <button className={styles.button} onClick={handleGoBack}>
-      Go Back
+    <button
+      className={`${styles.button} ${styles[theme]}`}
+      onClick={handleGoBack}
+      data-theme={theme}
+    >
+      {t('buttonText')}
     </button>
   );
 }
