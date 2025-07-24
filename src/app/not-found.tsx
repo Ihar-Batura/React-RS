d41f67f786fb8fa84aa7aa83c0b8e6796c08bff9
Ihar-Batura <A17331582@gmail.com>
2025-07-24 14:02:13 +0300

    refactor: make not-found.tsx server component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,37 +1,20 @@
-'use client';
+'use server';
 
-import styles from './not-found/NotFoundPage.module.scss';
-import { useTheme } from '../shared/hooks/useTheme';
-import { useRouter } from 'next/navigation';
+import styles from '../assets/styles/NotFoundPage.module.scss';
+import ButtonGoHome from '../components/ui/buttons/go-home';
+import ButtonGoBack from '../components/ui/buttons/go-back';
 
-export default function NotFoundPage() {
-  const { theme } = useTheme();
-  const router = useRouter();
-
-  const handleGoHome = () => {
-    router.push('/');
-  };
-
-  const handleGoBack = () => {
-    router.back();
-  };
-
+export default async function NotFoundPage() {
   return (
-    <>
-      <main className={`${styles.container} ${styles[theme]}`}>
-        <div className={styles.wrapper}>
-          <h2 className={styles.number}>404</h2>
-          <div className={styles.title}>This page doesn’t exist</div>
-          <div className={styles.buttonsContainer}>
-            <button className={styles.button} onClick={handleGoHome}>
-              Go Home
-            </button>
-            <button className={styles.button} onClick={handleGoBack}>
-              Go Back
-            </button>
-          </div>
+    <main className={`${styles.container} `}>
+      <div className={styles.wrapper}>
+        <h2 className={styles.number}>404</h2>
+        <div className={styles.title}>This page doesn’t exist</div>
+        <div className={styles.buttonsContainer}>
+          <ButtonGoHome />
+          <ButtonGoBack />
         </div>
-      </main>
-    </>
+      </div>
+    </main>
   );
 }
