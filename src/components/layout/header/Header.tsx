3165cc3138d1d65c8ce5bf734e05f7268d28117f
Ihar-Batura <A17331582@gmail.com>
2025-08-21 14:21:48 +0300

    feat: add open form in header

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,12 +1,42 @@
 import styles from './Header.module.css';
+import { useState } from 'react';
+import { Modal } from '../../modal/Modal';
+import { HookForm } from '../../forms/HookForm';
 
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpenHookForm, setIsOpenHookForm] = useState(false);
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <button className={styles.button}>Form 1</button>
-        <button className={styles.button}>Form 2</button>
+        <button
+          className={styles.button}
+          onClick={() => {
+            setIsOpen(true);
+            setIsOpenHookForm(false);
+          }}
+        >
+          Form 1
+        </button>
+        <button
+          className={styles.button}
+          onClick={() => {
+            setIsOpen(true);
+            setIsOpenHookForm(true);
+          }}
+        >
+          Form 2
+        </button>
       </div>
+      <Modal
+        isOpen={isOpen}
+        onClose={() => {
+          setIsOpen(false);
+        }}
+      >
+        {isOpenHookForm ? <HookForm /> : <div>Form 111</div>}
+      </Modal>
     </header>
   );
 };
